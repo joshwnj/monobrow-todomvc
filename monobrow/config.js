d4be22dfa31056f6d5017123b70c8af2db8612f7
Josh Johnston <josh@x-team.com>
2017-01-06 18:15:19 +1100

    upgraded monobrow

--- monobrow/config.js
+++ monobrow/config.js
@@ -1,26 +1,7 @@
-const babelify = require('babelify')
-const path = require('path')
-
-const browserDeps = require('./deps').modules
-
-function setup (b, opts) {
-  b.external(browserDeps)
-
-  b.transform(babelify, {
-    plugins: [
-      require('babel-plugin-transform-class-properties'),
-      require('babel-plugin-transform-object-rest-spread')
-    ],
-
-    presets: [
-      require('babel-preset-latest'),
-      require('babel-preset-react')
-    ]
-  })
-}
-
 module.exports = {
   entry: 'src/index.js',
-  outDir: 'build',
-  setup: setup
+  output: {
+    dir: 'build'
+  },
+  packs: [ require('./pack') ]
 }
